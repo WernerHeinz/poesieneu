49ad1ac83a17c373ecdb2d5bf141c94aa0fe7541
WernerHeinz <45233394+WernerHeinz@users.noreply.github.com>
2019-02-04 08:51:24 +0100

    Update Inhalt.js

--- src/components/Inhalt.js
+++ src/components/Inhalt.js
@@ -3,8 +3,8 @@ import React from 'react'
 import {InhaltsDiv} from './Format'
 export const Inhalt = () => (
     <InhaltsDiv>
-        <h1>Heine kommt wieder! - Im August 2019"</h1>
-            iii
+        <h1>Heine kommt wieder! - Im August 2019</h1>
+            
         
         <img src="/ufer-paddel.jpg" alt="Titelbild" />
         
