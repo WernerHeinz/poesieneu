0eaefad7d75af21a88c7ea1d2a382e333e10412a
WernerHeinz <45233394+WernerHeinz@users.noreply.github.com>
2019-02-06 23:52:49 +0100

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -8,7 +8,7 @@ const BlogPost = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: flex-start;
-align-items: flex-start;
+align-items: center;
 padding:0 20px 0 20px;  
 height:82vh;
 
