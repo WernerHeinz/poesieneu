1ccffb5c03c02089792e90060236395af8c8020d
WernerHeinz <45233394+WernerHeinz@users.noreply.github.com>
2019-02-07 01:29:45 +0100

    Update Inhalt.js

--- src/components/Inhalt.js
+++ src/components/Inhalt.js
@@ -1,13 +1,16 @@
 import React from 'react'
-
+import Layout from './layout'
 import {InhaltsDiv} from './Format'
+import Layout from './layout';
 export const Inhalt = () => (
+    <Layout>
     <InhaltsDiv>
         <h1>Heine kommt wieder! - Im August 2019</h1>
-            
+           
         
         <img src="/ufer-paddel.jpg" alt="Titelbild" />
         
-    </InhaltsDiv>     
+    </InhaltsDiv>
+    </Layout>     
     
     )
