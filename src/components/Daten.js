99bdb8f7891b6772c6e38b20996ea18eb19a4a91
WernerHeinz <45233394+WernerHeinz@users.noreply.github.com>
2019-02-08 12:55:03 +0100

    Update Daten.js

--- src/components/Daten.js
+++ src/components/Daten.js
@@ -2,36 +2,28 @@
 import React from 'react'
 
 import {Link } from 'gatsby'
-import UserContext from '../components/UserContext'
+
 import {Listen,MyButton,Dschutz} from '../components/Format'
 import {Inhalt} from '../components/Inhalt'
 
  const Daten = (props) => {
     return(
-        <UserContext.Consumer>{
-            ({dflag}) => {
-                console.log(dflag)
-                
-            return(
+
                 <React.Fragment>
                 <Listen>
                     <Inhalt/>
                 </Listen>
-           <Dschutz timeout={890} in={!dflag} unmountOnExit >
+               <Dschutz>
                 <h3>Auch wir nutzen <br/>Cookies und ...</h3>
                 <ul>
-                   
+
                     <li><Link to ="/datenschutz">Weitere Informationen</Link><br/></li>
                     <li><MyButton onClick={props.toggleDflag}>Schon zugestimmt ...</MyButton></li>
                     <li><MyButton onClick={props.toggleDflag}>Ich stimme zu.</MyButton></li>
                 </ul>
-           </Dschutz>
-        </React.Fragment> 
+        </Dschutz>
+        </React.Fragment>
             )
-            }
-        }
-       
-        </UserContext.Consumer>
-    )
+
 }
 export default Daten
