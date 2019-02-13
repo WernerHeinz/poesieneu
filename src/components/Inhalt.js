3da490741cce1440b4eb01e7b9ea742650dae384
WernerHeinz <45233394+WernerHeinz@users.noreply.github.com>
2019-02-14 00:35:27 +0100

    Update Inhalt.js

--- src/components/Inhalt.js
+++ src/components/Inhalt.js
@@ -6,23 +6,20 @@ export const Inhalt = () => (
     <Layout>
     <Listen>
          <Liste>
+       <div style={{padding:'20px',textAlign:'center'}}>
         <h1>Heine kommt wieder! - Im August 2019</h1>
            
         
         <img src="/ufer-paddel.jpg" alt="Titelbild" />
-        <p>2015 und 2017 haben wir unser inzwischen legendäres Heine-Projekt, 
-            mit dem wir 2013  mehr als 3000 Zuschauer zu sieben Spielstätten auf den  Fundamenten der 
-            Frankfurter Judengasse geführt hatten, mit großem Erfolg nach Bacharach am Rhein übertragen. </p>
-<p>Der viereinhalbstündige theatrale Parcours führt dort von der Ruine der St. Werner-Kapelle zum Rheinufer 
-und weiter zu fünf außergewöhnlichen Spielorten im schönen Rheinstädtchen. </p>
-<p>Im vergangenen Jahr haben wir Heines „Rabbi von Bacharach“ um weitere Inszenierungen in den benachbarten Städtchen Lorch und Oberwesel zu einem Festival  „An den Ufern der Poesie“ erweitert, das zufünftig in biennalem Turnus im Weltkulturerbe Oberes Mittelrheintal stattfinden soll.</p>
-<p>Und jetzt stecken wir mitten in den Planungen des Festivals 2019. An zwei Wochenenden im August 2019 wird es vier Aufführungen des „Rabbi von Bacharach“ geben. </p>
-   <p> Unsere Freundinnen und Freunde vom Frankfurter Schauspiel-Ensemble werden – ebenfalls im August - ihre Inszenierung von Christa Wolfs „Kein Ort. Nirgends“ an den Rhein mitbringen und auch Michael Quast wird  mit seiner Fliegenden Volksbühne an den Ufern der Poesie im Weltkulturerbe landen. 
-<b>Bitte den August im Kalender vormerken. Wir halten Sie auf dem Laufenden.*
-</b></p>
-        
+          
+        <p style={{fontWeight:'600'}}>10. AUGUST - 1. SEPTEMBER 2019 </p> 
+        <p style={{fontWeight:'600'}}>THEATERFESTIVAL FÜR RHEINSÜCHTIGE MELANCHOLIKER <br/>im Welterbe Oberes Mittelrheintal</p> 
+       
+        </div>
      </Liste>    
     </Listen>
     </Layout>     
     
     )
+    
+  
