f56339957b7f07d1949764da65753c8e1a891dcd
WernerHeinz <45233394+WernerHeinz@users.noreply.github.com>
2019-02-13 19:45:51 +0100

    Update Format.js

--- src/components/Format.js
+++ src/components/Format.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 
 
-export const InhaltsDiv = styled.div`
+export const InDiv = styled.div`
 display:flex;
 flex-direction:column;
 justify-content:flex-start;
