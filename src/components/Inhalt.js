123316e5e3924dd09235cf34528af66510111766
WernerHeinz <45233394+WernerHeinz@users.noreply.github.com>
2020-02-24 22:25:44 +0100

    Update Inhalt.js

--- src/components/Inhalt.js
+++ src/components/Inhalt.js
@@ -7,13 +7,13 @@ export const Inhalt = () => (
      <Listen>
      
        
-        <h1>Heine kommt wieder! - Im August 2019</h1>
+        <h1>Heine kommt wieder! - Im Juni 2021</h1>
            
         
         <img src="/ufer-paddel.jpg" alt="Titelbild" />
           
-        <p style={{fontWeight:'600'}}>10. AUGUST - 1. SEPTEMBER 2019 </p> 
-        <p style={{fontWeight:'600'}}>THEATERFESTIVAL FÜR RHEINSÜCHTIGE MELANCHOLIKER <br/>im Welterbe Oberes Mittelrheintal</p> 
+        <p style={{fontWeight:'600'}}>geplant: 18.Juni bis 18. Juli 2021</p> 
+        <p style={{fontWeight:'600'}}>THEATERFESTIVAL IM BIENNALEN TURNUS<br/>im Welterbe Oberes Mittelrheintal</p> 
        
        
        
