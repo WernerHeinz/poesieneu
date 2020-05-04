5f69387f9a1489809d9a2364910ee7f02baa7309
WernerHeinz <45233394+WernerHeinz@users.noreply.github.com>
2020-05-04 21:01:18 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,13 +43,13 @@ class Menu extends React.Component{
          <li><Link to="/heine">Heine kehrt zurück. <br/></Link></li>
          <li><Link to="/aktuell">Aktuelles</Link></li>
          <li><Link to="/archiv">Filme und Foto-Dokumentation</Link></li>
-         <li><Link to="/termine">Tickets und Termine</Link></li>
          <li><Link to="/stimmen">Stimmen und Kommentare zum Festival</Link></li>
+         <li><Link to="/presse">Presse 2019</Link></li>
          <li><Link to="/programm">Programm 2019</Link> </li>
-         <li><Link to="/links">Sponsoren-Unterstützer-Partner</Link></li>
-         <li><Link to="/unterkunft-pauschalen">Unterkunft-Pauschalen</Link></li>
+         <li><Link to="/termine">Tickets und Termine 2019</Link></li>
+         <li><Link to="/links">Sponsoren-Unterstützer-Partner 2019</Link></li>
+         <li><Link to="/unterkunft-pauschalen">Unterkunft-Pauschalen 2019</Link></li>
          <li><Link to="/praml">Who is Who im Festival?</Link></li>
-         <li><Link to="/presse">Presse</Link></li>
          <li><Link to="/kontakt">Kontakt</Link></li>
          <li><Link to="/datenschutz">Datenschutz</Link></li>
          <li><Link to="/impressum">Impressum</Link></li>
